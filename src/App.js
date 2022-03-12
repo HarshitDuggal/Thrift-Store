5d4b66cd03bbd7f42fe19861da1b3cc20c8b127c
Harshit Duggal <harshitduggal101@gmail.com>
2022-03-12 23:22:36 +0530

    syntax update

--- src/App.js
+++ src/App.js
@@ -2,30 +2,33 @@ import "./App.css";
 import { useState } from "react";
 import Home from "./Pages/Landing/home";
 import db from "./Data/db.json";
-import users from "./Data/users.json"
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import users from "./Data/users.json";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login/login";
 // import Navbar from "./Components/Navbar/navbar";
 
 function App() {
   const [data, setdata] = useState(db);
   return (
-    // <Router>
-    //   <Navbar/>
-    //   <div className="App">
-    //     <Switch>
-    //       <Route exact path="/"  >
-    //         <Home data={data}/>
-    //         </Route>
-    //       <Route exact path="/login"  component={Login}/>
-    //     </Switch>
-    //   </div>
-    // </Router>
-    <div className="App">
-      <Home data={data} />
-      <Login users={users}/>
-    </div>
-      );
+    <Router>
+      {/* <Navbar /> */}
+      <div className="App">
+        <Routes>
+          {/* <Route exact path="/">
+            <Home data={data} />
+          </Route> */}
+          {/* <Route exact path="/login" component={Login} /> */}
+          <Route path="/" element={<Home/>} />
+
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </div>
+    </Router>
+    // <div className="App">
+    //   <Home data={data} />
+    //   <Login users={users} />
+    // </div>
+  );
 }
 
 export default App;
